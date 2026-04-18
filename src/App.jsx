f21dd27a550199bd26e61a65ca6e1f1ec42e43c7
MBello21 <magarbello@gmail.com>
2026-04-18 03:48:52 +0200

    Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import Logo from "../public/Logo.png"
+
 
 export const App = () => {
     return (
-        <div className="d-flex flex-column justify-content-center alig-items-center ">
-            <h1 className="text-light-emphasis">Próximamente...</h1>
-            <p className="text-light text-center">
-                Algo increíble está por llegar. Deja tu correo y sé el primero en enterarte del gran estreno.
-            </p>
-            <div className="d-flex justify-content-center ">
-                <a className="btn btn-outline-light w-25"
-                    href="mailto:info@mgbdevops.es?subject=Consulta desde la web en construcción">
-                    Quiero saber más
-                </a>
+        <>
+
+            <div className="container-fluid">
+                <img src={Logo} alt="" />
             </div>
-        </div>
+            <div className="d-flex flex-column justify-content-center alig-items-center ">
+                <h1 className="text-light-emphasis">
+                    Próximamente...
+                </h1>
+                <p className="text-light">
+                    Algo increíble está por llegar. Deja tu correo y sé el primero en enterarte del gran estreno.
+                </p>
+                <div className="d-flex justify-content-center ">
+                    <a className="btn btn-outline-light w-25"
+                        href="mailto:info@mgbdevops.es?subject=Consulta desde la web en construcción">
+                        Quiero saber más
+                    </a>
+                </div>
+            </div>
+        </>
     )
 }
